78a78feccecd656cc0ff875d56781a13caa87727
Shamal Kumbhar <kumbharshamal317@gmail.com>
2025-02-28 14:13:11 +0530

    Add files via upload

---
+++ Even number.py
@@ -0,0 +1,15 @@
+# no=input("Enter any number")
+# no=int(no)
+
+# to print even and odd numbers
+
+no=int(input("Enter any number"))
+if no%2==0:
+    print("Even number")
+else:
+    print("Odd number")
+
+'''
+number is divisible by 3 and 5
+if no%3 and no%5
+'''
