90a8eef7bf192b8a328dfaf094fa041a7bc1b90f
Shamal Kumbhar <kumbharshamal317@gmail.com>
2025-02-28 14:12:07 +0530

    Add files via upload

---
+++ Hello Word.py
@@ -0,0 +1,11 @@
+# to print hello world
+
+print("Hello Word")
+
+print("Good Morning")
+print("Hello word",end='\t')
+
+# To print name,age
+name=input("Enter Your name")
+age=int(input("Enter your age"))
+print("Hello",name,"Your age is: ",age)
